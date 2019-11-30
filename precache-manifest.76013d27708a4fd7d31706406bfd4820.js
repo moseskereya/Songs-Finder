f401023e87d28796835d2c5f243e34fce692146f
MOSES <MOSESLAZARO2019@outlook.com>
2019-11-30 23:23:22 +0300

    Updates

---
+++ precache-manifest.76013d27708a4fd7d31706406bfd4820.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e753eb35a01fbf05cc13e29e366a29a",
+    "url": "/Songs-Finder/index.html"
+  },
+  {
+    "revision": "168d21486608eacdc327",
+    "url": "/Songs-Finder/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "03a5e2c0ea16dfee107b",
+    "url": "/Songs-Finder/static/js/2.abf34b95.chunk.js"
+  },
+  {
+    "revision": "168d21486608eacdc327",
+    "url": "/Songs-Finder/static/js/main.83db1f8e.chunk.js"
+  },
+  {
+    "revision": "9fba38a6e451ca51112c",
+    "url": "/Songs-Finder/static/js/runtime-main.e91f55f7.js"
+  }
+]);
